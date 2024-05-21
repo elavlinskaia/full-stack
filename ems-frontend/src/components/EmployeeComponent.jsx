57fd1f3f6b08df87fd76f0fbdc0b6685a384a462
Екатерина Лавлинская <lavlinskaia.e@gmail.com>
2024-05-21 14:46:46 +0300

    first commit

---
+++ ems-frontend/src/components/EmployeeComponent.jsx
@@ -0,0 +1,162 @@
+import React, { useEffect, useState } from 'react'
+import { createEmployee, getEmployee, updateEmployee } from '../services/EmployeeService'
+import { useNavigate, useParams } from 'react-router-dom';
+
+const EmployeeComponent = () => {
+
+    const [firstName, setFirtName] = useState('')
+    const [lastName, setLastName] = useState('')
+    const [email, setEmail] = useState('')
+
+    const {id} = useParams();
+
+    const [errors, setErrors] = useState({
+        firstName: '',
+        lastName: '',
+        email: ''
+    })
+
+    const navigator = useNavigate();
+
+    useEffect(() => {
+        if (id) {
+            getEmployee(id).then((response) => {
+                setFirtName(response.data.firstName);
+                setLastName(response.data.lastName);
+                setEmail(response.data.email);
+            }).catch(error => {
+                console.error(error);
+            })
+        }
+    }, [id])
+
+    function saveOrUpdateEmployee(e) {
+        e.preventDefault();
+
+        if (validateForm()) {
+
+            const employee = {firstName, lastName, email}
+            console.log(employee)
+
+            if (id) {
+                updateEmployee(id, employee).then((response) => {
+                    console.log(response.data);
+                    navigator('/employees');
+                }).catch(error => {
+                    console.error(error);
+                })
+            } else {
+                createEmployee(employee).then((response) => {
+                    console.log(response.data);
+                    navigator('/employees')
+                }).catch(error => {
+                    console.error(error);
+                })
+            }
+        }
+    }
+
+    function validateForm() {
+        let valid = true;
+
+        const errorsCopy = {... errors}
+
+        if (firstName.trim()) {
+            errorsCopy.firstName = '';
+        } else {
+            errorsCopy.firstName = 'Имя обязательно';
+            valid = false;
+        }
+
+        if (lastName.trim()) {
+            errorsCopy.lastName = '';
+        } else {
+            errorsCopy.lastName = 'Фамилия обязательна';
+            valid = false;
+        }
+
+        if (email.trim()) {
+            errorsCopy.email = '';
+        } else {
+            errorsCopy.email = 'Email обязателен';
+            valid = false;
+        }
+
+        setErrors(errorsCopy);
+
+        return valid;
+    }
+
+    function pageTitle() {
+        if (id) {
+            return <h2 className='text-center'>Обновить</h2>
+        }
+        else {
+            return <h2 className='text-center'>Добавить сотрудника</h2>
+        }
+    }
+
+  return (
+    <div className='container'>
+        <br /> <br />
+        <div className='row'>
+            <div className='card col-md-6 offset-md-3 offset-md-3'>
+                {
+                    pageTitle()
+                }
+                <div className='card-body'>
+                    <form>
+                        <div className='form-group mb-2'>
+                            <label className='form-label'>Имя:</label>
+                            <input
+                                type='text'
+                                placeholder='Введите имя'
+                                name='firstName'
+                                value={firstName}
+                                className={`form-control ${ errors.firstName ? 'is-invalid' : ''}`}
+                                onChange={(e) => setFirtName(e.target.value)}
+                            >
+                            </input>
+                            { errors.firstName && <div className='invalid-feedback'> {errors.firstName} </div>}
+                        </div>
+
+                        <div className='form-group mb-2'>
+                            <label className='form-label'>Фамилия:</label>
+                            <input
+                                type='text'
+                                placeholder='Введите фамилию'
+                                name='lastName'
+                                value={lastName}
+                                className={`form-control ${ errors.lastName ? 'is-invalid' : ''}`}
+                                onChange={(e) => setLastName(e.target.value)}
+                            >
+                            </input>
+                            { errors.lastName && <div className='invalid-feedback'> {errors.lastName} </div>}
+                        </div>
+
+                        <div className='form-group mb-2'>
+                            <label className='form-label'>Email:</label>
+                            <input
+                                type='text'
+                                placeholder='Введите email'
+                                name='lastName'
+                                value={email}
+                                className={`form-control ${ errors.email ? 'is-invalid' : ''}`}
+                                onChange={(e) => setEmail(e.target.value)}
+                            >
+                            </input>
+                            { errors.email && <div className='invalid-feedback'> {errors.email} </div>}
+                        </div>
+
+                        <button className='btn btn-success' onClick={saveOrUpdateEmployee}>Submit</button>
+
+                    </form>
+
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default EmployeeComponent
